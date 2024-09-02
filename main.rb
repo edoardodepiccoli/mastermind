3ee056bd9dc8ba86dcf4b0d2e4e9f60b5cacc0b6
Edoardo De Piccoli <103844232+edoardodepiccoli@users.noreply.github.com>
2024-09-02 23:22:58 +0200

    add more roles options
    add options to play (guesser vs puzzler) human vs human,
    human vs ai, ai vs human

--- main.rb
+++ main.rb
@@ -2,17 +2,26 @@ require_relative 'lib/game'
 
 system("clear")
 
+roles_combinations = {
+  "a" => [AiPlayer, HumanPlayer],
+  "b" => [HumanPlayer, HumanPlayer],
+  "c" => [HumanPlayer, AiPlayer]
+}
+
 puts("welcome to mastermind!".colorize(:blue))
-puts("do you want to be the guesser or the puzzler? (g/p)")
+puts("choose how you want to play")
+puts("a) ai decides the secret row, you guess")
+puts("b) your friend decides the secret row, you guess")
+puts("c) you decide the secret row, ai guesses")
 
-user_role = gets.chomp
+user_choice = gets.chomp.downcase
 
-until user_role == "g" || user_role == "p" do
+until user_choice == "a" || user_choice == "b" || user_choice == "c" do
   puts("try again")
-  user_role = gets.chomp
+  user_choice = gets.chomp
 end
 
-puzzler_class = user_role == "p" ? HumanPlayer : AiPlayer
-guesser_class = user_role == "p" ? AiPlayer : HumanPlayer
+puzzler_class = roles_combinations[user_choice][0]
+guesser_class = roles_combinations[user_choice][1]
 
 game = Game.new(puzzler_class, guesser_class)
